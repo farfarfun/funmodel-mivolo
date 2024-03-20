93bd28dbdf168ae56044a8b87c6f5709d1638bce
farfun <1007530194@qq.com>
2024-03-20 19:01:46 +0800

    add

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from funpypi import setup
 
 
-install_requires = ["funpypi","funmodel",
+install_requires = [
+    "funpypi",
+    "funmodel",
+    "huggingface_hub",
+    "ultralytics==8.0.124",
+    "timm==0.8.13.dev0",
+    "yt_dlp",
+    "lapx>=0.5.2",
+]
 
 
-
-
-
-"huggingface_hub",
-"ultralytics==8.0.124",
-"timm==0.8.13.dev0",
-"yt_dlp",
-"lapx>=0.5.2"]
-
-
-
-
-setup(name="funmodel-mivolo", 
-description="Layer MiVOLO for SOTA age and gender recognition",
-python_requires=">=3.8",
-install_requires=install_requires)
+setup(
+    name="funmodel-mivolo",
+    description="Layer MiVOLO for SOTA age and gender recognition",
+    python_requires=">=3.8",
+    install_requires=install_requires,
+)
